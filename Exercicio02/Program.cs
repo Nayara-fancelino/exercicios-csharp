119e0d009fe7c4623cd9510f3042fa41184a126a
Nayara Privado Francelino <SEU_EMAIL_DO_GITHUB>
2026-08-16 09:17:37 -0300

    Adiciona exercício 02 - antecessor e sucessor

---
+++ Exercicio02/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Digite um número inteiro: ");
+int numero = int.Parse(Console.ReadLine());
+
+int antecessor = numero - 1;
+int sucessor = numero + 1;
+
+Console.WriteLine($"Antecessor: {antecessor}");
+Console.WriteLine($"Número informado: {numero}");
+Console.WriteLine($"Sucessor: {sucessor}");
