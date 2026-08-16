dad7909db592447a69074f9c26ab35f6492aa7ab
Nayara Privado Francelino <SEU_EMAIL_DO_GITHUB>
2026-08-16 10:01:27 -0300

    Adiciona exercício 12 - Contador de caracteres

---
+++ Exercicio12/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Digite seu nome: ");
+string nome = Console.ReadLine();
+
+int quantidadeCaracteres = nome.Length;
+
+Console.WriteLine($"O nome possui {quantidadeCaracteres} caracteres.");
