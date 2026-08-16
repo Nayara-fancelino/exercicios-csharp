088511648ab3c1b7b08a64158c765a26157e0ec2
Nayara Privado Francelino <SEU_EMAIL_DO_GITHUB>
2026-08-16 10:04:40 -0300

    Adiciona exercício 13 - Verificador de senha

---
+++ Exercicio13/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Digite uma senha: ");
+string senha = Console.ReadLine();
+
+if (senha.Length >= 8)
+{
+    Console.WriteLine("A senha possui pelo menos 8 caracteres.");
+}
+else
+{
+    Console.WriteLine("A senha possui menos de 8 caracteres.");
+}
