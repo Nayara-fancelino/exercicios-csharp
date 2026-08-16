5772e4a3db9476bc95dd82581e2715fae60bfbfd
Nayara Privado Francelino <SEU_EMAIL_DO_GITHUB>
2026-08-16 09:57:15 -0300

    Adiciona exercício 11 - Soma até digitar zero

---
+++ Exercicio11/Program.cs
@@ -0,0 +1,16 @@
+﻿int soma = 0;
+
+while (true)
+{
+    Console.Write("Digite um número (0 para encerrar): ");
+    int numero = int.Parse(Console.ReadLine());
+
+    if (numero == 0)
+    {
+        break;
+    }
+
+    soma = soma + numero;
+}
+
+Console.WriteLine($"A soma dos valores digitados é: {soma}");
