5cb9eb3a9bfac653db8c7d096ce37e614e7332ad
Nayara Privado Francelino <SEU_EMAIL_DO_GITHUB>
2026-08-16 10:08:00 -0300

    Adiciona exercício 14 - Maior elemento

---
+++ Exercicio14/Program.cs
@@ -0,0 +1,22 @@
+﻿int[] numeros = new int[10];
+
+for (int i = 0; i < 10; i++)
+{
+    Console.Write($"Digite o {i + 1}º número: ");
+    numeros[i] = int.Parse(Console.ReadLine());
+}
+
+int maior = numeros[0];
+int posicaoMaior = 0;
+
+for (int i = 1; i < 10; i++)
+{
+    if (numeros[i] > maior)
+    {
+        maior = numeros[i];
+        posicaoMaior = i;
+    }
+}
+
+Console.WriteLine($"Maior valor: {maior}");
+Console.WriteLine($"Posição: {posicaoMaior + 1}");
