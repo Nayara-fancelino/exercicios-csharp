94e45d6a80dc428637ed2b8f4db757482bc5c119
Nayara Privado Francelino <SEU_EMAIL_DO_GITHUB>
2026-08-16 10:26:01 -0300

    Adiciona exercício 18 - Jogo da senha

---
+++ Exercicio18/Program.cs
@@ -0,0 +1,21 @@
+﻿Random random = new Random();
+int numeroSorteado = random.Next(1, 101);
+
+int tentativa = 0;
+
+while (tentativa != numeroSorteado)
+{
+    Console.Write("Digite um número entre 1 e 100: ");
+    tentativa = int.Parse(Console.ReadLine());
+
+    if (tentativa < numeroSorteado)
+    {
+        Console.WriteLine("O número sorteado é maior.");
+    }
+    else if (tentativa > numeroSorteado)
+    {
+        Console.WriteLine("O número sorteado é menor.");
+    }
+}
+
+Console.WriteLine("Parabéns! Você acertou o número.");
