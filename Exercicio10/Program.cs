1d3d66009bdf9841c4bdcdb138f846c6b605fa7e
Nayara Privado Francelino <SEU_EMAIL_DO_GITHUB>
2026-08-16 09:53:53 -0300

    Adiciona exercício 10 - Login simples

---
+++ Exercicio10/Program.cs
@@ -0,0 +1,15 @@
+﻿string senhaCorreta = "1234";
+
+Console.Write("Digite o usuário: ");
+string usuario = Console.ReadLine();
+
+Console.Write("Digite a senha: ");
+string senha = Console.ReadLine();
+
+while (senha != senhaCorreta)
+{
+    Console.Write("Senha incorreta. Digite novamente: ");
+    senha = Console.ReadLine();
+}
+
+Console.WriteLine($"Login realizado com sucesso. Bem-vindo, {usuario}!");
