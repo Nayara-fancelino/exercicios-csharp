150ef3420e98cac343c996d17d1a4c3d7b79d5fe
Nayara Privado Francelino <SEU_EMAIL_DO_GITHUB>
2026-08-16 10:19:31 -0300

    Adiciona exercício 16 - Contagem de pares

---
+++ Exercicio16/Program.cs
@@ -0,0 +1,14 @@
+﻿int quantidadePares = 0;
+
+for (int i = 0; i < 20; i++)
+{
+    Console.Write($"Digite o {i + 1}º número: ");
+    int numero = int.Parse(Console.ReadLine());
+
+    if (numero % 2 == 0)
+    {
+        quantidadePares++;
+    }
+}
+
+Console.WriteLine($"Quantidade de números pares: {quantidadePares}");
