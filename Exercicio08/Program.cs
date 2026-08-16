7318c9ebeab3ed98cb9c36d3311e04912e6a5d10
Nayara Privado Francelino <SEU_EMAIL_DO_GITHUB>
2026-08-16 09:45:55 -0300

    Adiciona exercício 08 - Tabuada

---
+++ Exercicio08/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Digite um número: ");
+int numero = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= 10; i++)
+{
+    int resultado = numero * i;
+    Console.WriteLine($"{numero} x {i} = {resultado}");
+}
