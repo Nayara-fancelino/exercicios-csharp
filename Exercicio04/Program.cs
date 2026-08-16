3507b9f14909481491e823cece2e2a31bb8e29a6
Nayara Privado Francelino <SEU_EMAIL_DO_GITHUB>
2026-08-16 09:33:37 -0300

    Adiciona exercício 04 - conversor de moedas

---
+++ Exercicio04/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Digite o valor em reais: R$ ");
+double valorReais = double.Parse(Console.ReadLine());
+
+Console.Write("Digite a cotação atual do dólar: R$ ");
+double cotacaoDolar = double.Parse(Console.ReadLine());
+
+double valorDolares = valorReais / cotacaoDolar;
+
+Console.WriteLine($"Valor em dólares: US$ {valorDolares:F2}");
